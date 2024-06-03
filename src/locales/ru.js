ead718704ab468861f7f693df1303de225f38617
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-06-03 17:08:42 +0300

    add src/locales/

---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    form: {
+      link: 'ссылка RSS',
+      add: 'Добавить',
+    },
+    feedbackMessage: {
+      success: 'RSS успешно загружен',
+      alreadyExists: 'RSS уже существует',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRSS: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+    content: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      show: 'Просмотр',
+    },
+  },
+};
